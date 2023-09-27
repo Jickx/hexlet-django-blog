dff7367e092db4c449f604ce772cffffb0cb5fd5
Jickx <valentin.sharonov@gmail.com>
2023-09-27 20:45:07 +0300

    add article view

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 
-from hexlet_django_blog.article.models import Article
+from hexlet_django_blog.article.models import Article, Comment
 
 
 class IndexView(View):
-
     def get(self, request, *args, **kwargs):
         articles = Article.objects.all()[:15]
         return render(request, 'articles/index.html', context={
             'articles': articles,
+        })
+
+
+class ArticleView(View):
+
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs['id'])
+        return render(request, 'articles/show.html', context={
+            'article': article,
+        })
+
+
+class ArticleCommentsView(View):
+
+    def get(self, request, *args, **kwargs):
+        comment = get_object_or_404(Comment, id=kwargs['id'], article__id=kwargs['article_id'])
+        return render(request, 'article/comments.html', context={
+            'comment': comment,
         })
